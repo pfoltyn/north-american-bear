efc917944b810639c25b95bd1aad5d13c0b558d0
Piotr Foltyn <piotr.foltyn@gmail.com>
2015-03-12 22:56:03 +0000

    Add settings scene stub.

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -3,11 +3,13 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
-public class MainMenu : MonoBehaviour
+public class Settings : MonoBehaviour
 {
 	public GameObject[] choiceSlots;
 	public Renderer backgroundRenderer;
 	public AudioSource clickAudio;
+	public Mesh Empty;
+	public Mesh Ticked;
 
 	private Fader fader;
 	private float scrollSpeedY;
@@ -28,6 +30,18 @@ public class MainMenu : MonoBehaviour
 		}
     }
 
+	private void ToggleMesh(MeshFilter meshFilter)
+	{
+		if (meshFilter.mesh.vertexCount == Empty.vertexCount)
+		{
+			meshFilter.mesh = Ticked;
+		}
+		else
+		{
+			meshFilter.mesh = Empty;
+		}
+	}
+
 	void OnGUI()
 	{
 		if (once && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
@@ -40,24 +54,22 @@ public class MainMenu : MonoBehaviour
 				once = false;
 				clickAudio.Play();
 				Animator animator = hit.collider.GetComponent<Animator>();
+				MeshFilter meshFilter = animator.gameObject.GetComponent<MeshFilter>();
 				if (choiceSlots[0] == animator.gameObject)
 				{
-					animator.SetBool("Pressed", true);
-					fader.Stop(() => Application.LoadLevel("4letters"));
+					fader.Stop(() => Application.LoadLevel("menu"));
 				}
 				else if (choiceSlots[1] == animator.gameObject)
 				{
-					animator.SetBool("Pressed", true);
-					fader.Stop(() => Application.LoadLevel("5letters_easy"));
+					ToggleMesh(meshFilter);
 				}
 				else if (choiceSlots[2] == animator.gameObject)
 				{
-					animator.SetBool("Pressed", true);
-					fader.Stop(() => Application.LoadLevel("5letters"));
+					ToggleMesh(meshFilter);
 				}
-				else
+				else if (choiceSlots[3] == animator.gameObject)
 				{
-					fader.Stop(() => Application.LoadLevel("settings"));
+					ToggleMesh(meshFilter);
 				}
 			}
 		}
@@ -69,5 +81,10 @@ public class MainMenu : MonoBehaviour
 		float offsetX = Mathf.Cos(2 * Mathf.PI * Time.time * scrollSpeedX);
 		backgroundRenderer.material.mainTextureOffset = new Vector2(offsetY, offsetX);
 		backgroundRenderer.material.mainTextureScale = new Vector2(3 + offsetY, 3 + offsetX);
+
+		if (Input.touchCount == 0)
+		{
+			once = true;
+		}
     }
 }
