dec4d7e73f1971d7ce07301cec06560b8b22ec3d
Piotr Foltyn <piotr.foltyn@gmail.com>
2015-03-15 17:04:03 +0000

    Add game continuation screen.

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -3,11 +3,15 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
-public class MainMenu : MonoBehaviour
+public class LevelLoader : MonoBehaviour
 {
+	public static Mesh titleMesh;
+	public static string levelToLoad;
+
 	public GameObject[] choiceSlots;
 	public Renderer backgroundRenderer;
 	public AudioSource clickAudio;
+	public MeshFilter titleMeshFilter;
 
 	private Fader fader;
 	private float scrollSpeedY;
@@ -28,6 +32,9 @@ public class MainMenu : MonoBehaviour
 			Animator animator = slot.GetComponent<Animator>();
 			animator.SetFloat("Speed", Random.Range(0f, 1f));
 		}
+
+		titleMeshFilter.mesh = titleMesh;
+
 		sounds = PlayerPrefs.GetInt("sounds", 1) == 1;
 		movingBackground = PlayerPrefs.GetInt("moving_background", 1) == 1;
     }
@@ -47,32 +54,20 @@ public class MainMenu : MonoBehaviour
 					clickAudio.Play();
 				}
 				Animator animator = hit.collider.GetComponent<Animator>();
-				string levelToLoad = "settings";
 				if (choiceSlots[0] == animator.gameObject)
 				{
-					animator.SetBool("Pressed", true);
-					levelToLoad = "4letters";
+					fader.Stop(() => Application.LoadLevel("menu"));
 				}
 				else if (choiceSlots[1] == animator.gameObject)
 				{
-					animator.SetBool("Pressed", true);
-					levelToLoad = "5letters_easy";
+					PlayerPrefs.SetInt(levelToLoad + "wordIndex", 0);
+					PlayerPrefs.SetInt(levelToLoad + "seed", Random.Range(0, 255));
+					PlayerPrefs.SetInt(levelToLoad + "score", 0);
+					fader.Stop(() => Application.LoadLevel(levelToLoad));
 				}
 				else if (choiceSlots[2] == animator.gameObject)
 				{
-					animator.SetBool("Pressed", true);
-					levelToLoad = "5letters";
-				}
-
-				LevelLoader.levelToLoad = levelToLoad;
-				LevelLoader.titleMesh = hit.collider.GetComponent<MeshFilter>().mesh;
-				if (PlayerPrefs.GetInt(levelToLoad + "score", 0) == 0)
-				{
 					fader.Stop(() => Application.LoadLevel(levelToLoad));
-				}
-				else
-				{
-					fader.Stop(() => Application.LoadLevel("level_loader"));
 				}
 			}
 		}
@@ -86,6 +81,11 @@ public class MainMenu : MonoBehaviour
 			float offsetX = Mathf.Cos(2 * Mathf.PI * Time.time * scrollSpeedX);
 			backgroundRenderer.material.mainTextureOffset = new Vector2(offsetY, offsetX);
 			backgroundRenderer.material.mainTextureScale = new Vector2(3 + offsetY, 3 + offsetX);
+		}
+
+		if (Input.touchCount == 0)
+		{
+			once = true;
 		}
     }
 }
